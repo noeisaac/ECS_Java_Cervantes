6b6b2ad251dfc2274e9d6966361474f46dd40968
noeisaac <CervantesN4621@sduhsd.lan>
2017-12-04 11:12:33 -0800

    worked one parat 1

---
+++ ECS_Java_Unit1/src/GoodFirstProgram.java
@@ -0,0 +1,18 @@
+
+public class GoodFirstProgram {
+
+	public static void main(String[] args) 
+	{
+		System.out.println( "Hello World!" );
+		System.out.println( "Hello Again" );
+		System.out.println( "I like typing this." );
+		System.out.println( "This is fun." );
+		System.out.println( "Yay! Printing." );
+		System.out.println( "I'd much rather you 'not'." );
+		System.out.println( " I \"said\" do not touch this." );
+		//System.out.println( "She likes applejacks. I like apple sauce." );
+	
+
+	}
+
+}
---
+++ ECS_Java_Unit1/src/YourInitials.java
@@ -0,0 +1,18 @@
+
+public class YourInitials {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println( "N   N   CCC " );
+		System.out.println( "NN  N  C   C" );
+		System.out.println( "N N N  C    " );
+		System.out.println( "N  NN  C    " );
+		System.out.println( "N   N  C    " );
+		System.out.println( "N   N  C   C" );
+		System.out.println( "N   N   CCC " );
+		
+
+	}
+
+}
