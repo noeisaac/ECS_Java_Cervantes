8e00b0a3a5545d489a8b0c521aba20d1814d5589
noeisaac <CervantesN4621@sduhsd.lan>
2017-12-06 10:32:03 -0800

    finished assignment 2

---
+++ Variables/src/StillUsingVariables.java
@@ -0,0 +1,20 @@
+
+public class StillUsingVariables {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int year;
+		String word;
+		
+		year = 2018;
+		word = "Noe Cervantes";
+		
+		System.out.println( "My name is " + word + " and I'll gtaduate in " + year + "." );
+		
+		
+		
+
+	}
+
+}
