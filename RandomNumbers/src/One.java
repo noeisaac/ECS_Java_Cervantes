5cfba87b3e160d49115dec35413a13f8789c8087
noeisaac <CervantesN4621@sduhsd.lan>
2017-12-11 11:04:17 -0800

    finished part one of assignmetn two

---
+++ RandomNumbers/src/One.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Random r = new Random();
+		int x = 1 + r.nextInt(10);
+		
+		System.out.println( " My random number is " + x );
+		System.out.println( " Here are some more numbers from 1 to 5!" );
+		System.out.println( 1 + r.nextInt(5) + " " );
+		System.out.println( 1 + r.nextInt(5) + " " );
+		System.out.println( 1 + r.nextInt(5) + " " );
+		System.out.println( 1 + r.nextInt(5) + " " );
+		System.out.println( 1 + r.nextInt(5) + " " );
+		System.out.println( 1 + r.nextInt(5) + " " );
+		System.out.println();
+		
+		System.out.println( "Here are some numbers from 1 to 100!" );
+		System.out.println( 1 + r.nextInt(100) + "/t" ); 
+		System.out.println( 1 + r.nextInt(100) + "/t" );
+		System.out.println( 1 + r.nextInt(100) + "/t" );
+		System.out.println();
+		
+		int num1 = 1 + r.nextInt(10);
+		int num2 = 1 + r.nextInt(10);
+		
+		if ( num1 == num2 )
+		{
+			System.out.println( "The random numbers were the same! Weird." );
+		}
+		if ( num1 != num2 )
+		{
+			System.out.println("The random numbers were different! Not too surprising, actually." );
+		}
+		
+				
+
+	}
+
+}
